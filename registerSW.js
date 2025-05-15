862b4adb365752f5630d73b564bc56fea7e89ef7
suprav-66 <142609701+suprav-66@users.noreply.github.com>
2025-05-16 01:57:33 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/bronotify/sw.js', { scope: '/bronotify/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
